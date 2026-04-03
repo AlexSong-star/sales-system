0baa5b1048e83737d95d5a840a16ada580ab4558
baizhangtang <baizhangtang@agent.ai>
2026-04-03 22:24:23 +0800

    perf: adjust vite chunk size warning threshold

--- code/frontend/vite.config.js
+++ code/frontend/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
   server: {
     proxy: {
       '/api': {
